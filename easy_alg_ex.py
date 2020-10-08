b1be72a64eb156253b4326b12ac7ecdfe0576709
GrumpycatInHeaven <vakulenko.mark2@gmail.comn>
2020-10-09 01:55:35 +0300

    Examples Binary search

--- easy_alg_ex.py
+++ easy_alg_ex.py
@@ -9,16 +9,46 @@ def binary_search(list, item):
         guess = list[mid]
         count += 1
         if guess == item:
-            return mid
+            return mid, count
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
-    return None
+    return None, count
 
 n = 128
 my_list = []
 for i in range(1,n+1):
     my_list.append(i)
 print(my_list)
-print(binary_search(my_list, 9))
+# Макс. кол-во проверок достигается макс. значением переменной
+print(binary_search(my_list, n))
+# Ответ: 8
+
+#1.2
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    count = 0
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        count += 1
+        if guess == item:
+            return mid, count
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None, count
+
+n = 128 * 2
+my_list = []
+for i in range(1,n+1):
+    my_list.append(i)
+print(my_list)
+# Макс. кол-во проверок достигается макс. значением переменной
+print(binary_search(my_list, n))
+# Ответ: 9
+
+# 1.3
