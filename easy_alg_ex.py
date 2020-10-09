e1e9a26dd4c5b4428f2f5a3d4bc069d8ec56f4c4
GrumpycatInHeaven <vakulenko.mark2@gmail.comn>
2020-10-09 03:12:08 +0300

    To be continue#1

--- easy_alg_ex.py
+++ easy_alg_ex.py
@@ -1,6 +1,6 @@
 # Бинарный поиск:
 # 1.1
-def binary_search(list, item):
+def binary_search1(list, item):
     low = 0
     high = len(list) - 1
     count = 0
@@ -16,17 +16,17 @@ def binary_search(list, item):
             low = mid + 1
     return None, count
 
-n = 128
-my_list = []
-for i in range(1,n+1):
-    my_list.append(i)
-print(my_list)
+n1 = 128
+my_list1 = []
+for i in range(1,n1 + 1):
+    my_list1.append(i)
+print(my_list1)
 # Макс. кол-во проверок достигается макс. значением переменной
-print(binary_search(my_list, n))
+print(binary_search1(my_list1, n1))
 # Ответ: 8
 
 #1.2
-def binary_search(list, item):
+def binary_search2(list, item):
     low = 0
     high = len(list) - 1
     count = 0
@@ -42,13 +42,13 @@ def binary_search(list, item):
             low = mid + 1
     return None, count
 
-n = 128 * 2
-my_list = []
-for i in range(1,n+1):
-    my_list.append(i)
-print(my_list)
+n2 = 128 * 2
+my_list2 = []
+for i in range(1,n2 + 1):
+    my_list2.append(i)
+
 # Макс. кол-во проверок достигается макс. значением переменной
-print(binary_search(my_list, n))
+print(binary_search2(my_list2, n2))
 # Ответ: 9
 
 # 1.3
